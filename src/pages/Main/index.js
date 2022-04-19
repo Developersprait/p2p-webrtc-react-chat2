9a3cb78e73c10c9008c2f088fa4b34fcbc4ac89e
Leon Antoshyn <Doctorsprait@gmail.com>
2022-04-19 13:41:45 +0300

    websocketes doesn't work

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,35 @@
+import {useState, useEffect} from 'react';
 import socket from '../../socket';
+import ACTIONS from '../../socket/actions';
+import {useHistory} from 'react-router-dom';
+import {v4} from 'uuid';
 
 export default function Main() {
+    const history = useHistory();
+    const [rooms, updateRooms] = useState([]);
+
+    useEffect(()=>{
+        socket.on(ACTIONS.SHARE_ROOMS, ({rooms = []} = {}) => {
+          updateRooms(rooms);
+        })
+    }, []);
     return(
-        <div>Main</div>
+        <div>
+            <h1>Available Rooms</h1>
+            
+            <ul>
+                {rooms.map(roomID => (
+                    <li key={roomID}>
+                        {roomID}
+                        <button onClick={() =>{
+                            history.push(`/room/${roomID}`);
+                        }}>JOIN ROOM</button>
+                    </li>
+                ))}
+            </ul>
+            <button onClick={() => {
+                history.push(`/room/${v4()}`)
+            }}>Create New Room</button>
+        </div>
     );
 };
